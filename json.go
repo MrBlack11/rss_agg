505046e0b0cc568976933eecb47e0c9e960f7dd1
Daniel Oliveira <daniel99955.04@gmail.com>
2023-08-07 12:38:53 -0300

    finishing boilerplate

--- json.go
+++ json.go
@@ -6,10 +6,24 @@ import (
 	"net/http"
 )
 
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	if code > 499 {
+		log.Println("Responding with 5xx error:", message)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJson(w, code, errResponse{
+		Error: message,
+	})
+}
+
 func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 	if err != nil {
-		log.Println("Failed to marshal JSON response: %v", payload)
+		log.Printf("Failed to marshal JSON response: %v", payload)
 		w.WriteHeader(500)
 		return
 	}
